f5fffe09fbf0ae3faf3caa6275f09717c8a26fca
唐天博 <gd523718@gmail.com>
2023-05-18 16:02:00 +0800

    Deno blog init ~

---
+++ main.tsx
@@ -0,0 +1,20 @@
+/** @jsx h */
+
+import blog, { ga } from "blog";
+
+blog({
+  author: "NoirVoider",
+  title: "NoirVoider Blog",
+  description: "A boring blog",
+  avatar: "https://www.gravatar.com/avatar/a164966f7def242e279fa521c323d106?s=300",
+  favicon: "https://www.gravatar.com/avatar/a164966f7def242e279fa521c323d106?s=50",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Email", url: "mailto:tangtianbo25@outlook.com" },
+    { title: "GitHub", url: "https://github.com/NoirVoider" },
+    { title: "Twitter", url: "https://twitter.com/b7o5m" }
+  ],
+  lang: "en",
+  dateFormat: date => new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
+  middlewares: [ga("UA-XXXXXXXX-X")]
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,14 @@
+---
+title: Hello world!
+publish_date: 2023-05-18
+---
+
+This is my first blog post!
+
+```python
+print("hello world")
+```
+
+```ts
+console.log("Welcome to Deno!");
+```
